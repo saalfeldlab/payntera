917b21d054c8af7b3c00231072b234afe79e8d0d
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-16 08:43:22 -0400

    Add first working version

--- payntera/jfx.py
+++ payntera/jfx.py
@@ -39,28 +39,17 @@ def init_platform():
     _get_jfx_util().platformImplStartup()
 
 def start_stage(root):
-    print(1)
     r = _get_runnable(lambda : _start_stage(root))
-    # r = _get_runnable(lambda : print("LOLOLOLOL"))
-    # print(1.1)
     jfx_application_thread = _get_invoke_on_jfx_application_thread()
-    # print(dir(jfx_application_thread))
-    # print(1.2)
-    # try:
     jfx_application_thread.invokeAndWait(r)
-    # except Exception as e:
-    #     print("WTF", e)
-    #     raise e
-    # print(1.3)
+    return root.getScene(), root.getScene().getWindow()
+
+def invoke_on_jfx_application_thread(func):
+    _get_invoke_on_jfx_application_thread().invoke(_get_runnable(func))
 
 def _start_stage(root):
-    print(2)
     scene = _get_scene()(root)
-    print(3)
     stage = _get_stage()()
-    print(4)
     stage.setScene(scene)
-    print(5)
     stage.show()
-    print(6)
     
--- test.py
+++ test.py
@@ -1,23 +1,51 @@
+import numpy as np
 import payntera
 import payntera.jfx
 import time
 import traceback
 
-
+import imglyb
 from jnius import autoclass, JavaException
 
 
+payntera.jfx.init_platform()
 PainteraBaseView = autoclass('org.janelia.saalfeldlab.paintera.PainteraBaseView')
 try:
     viewer           = PainteraBaseView.defaultView()
 except JavaException as e:
     print(e)
     print("inner message", e.innermessage)
+    if e.stacktrace:
+        for l in e.stacktrace:
+            print(l)
     raise e
 
-payntera.jfx.init_platform()
-print(dir(viewer))
-payntera.jfx.start_stage(viewer.baseView.pane())
+scene, stage = payntera.jfx.start_stage(viewer.paneWithStatus.getPane())
+
+LabelSourceState = autoclass('org.janelia.saalfeldlab.paintera.state.LabelSourceState')
+
+arr    = np.random.randint(100,size=(300,200,100))
+max_id = np.max(arr)
+img    = imglyb.to_imglib(arr)
+
+
+state = LabelSourceState.simpleSourceFromSingleRAIWithoutMeshes(
+    img,
+    [1.0, 1.0, 1.0],
+    [0.0, 0.0, 0.0],
+    max_id,
+    'bla',
+    viewer.baseView.viewer3D().meshesGroup(),
+    viewer.baseView.getMeshManagerExecutorService(),
+    viewer.baseView.getMeshWorkerExecutorService()
+    )
+
+payntera.jfx.invoke_on_jfx_application_thread( lambda : viewer.baseView.addLabelSource( state ) )
+
+viewer.keyTracker.installInto(scene)
+scene.addEventFilter(autoclass('javafx.scene.input.MouseEvent').ANY, viewer.mouseTracker)
+
+
 
 while True:
     time.sleep(0.5)
