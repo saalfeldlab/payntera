0b22daf75c2d427e7dd4f40675e22339ad1fca14
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-11-13 08:51:37 -0500

    Use scyjava for jar/endpoint management

--- payntera/__init__.py
+++ payntera/__init__.py
@@ -2,34 +2,36 @@ from .version import __version__
 
 import os
 
-__paintera_version__ = os.getenv('PAINTERA_VERSION', __version__)
-__jfx_rt_jar         = os.getenv('JFX_RT_JAR', os.path.join(os.getenv('JAVA_HOME'), 'jre', 'lib', 'ext', 'jfxrt.jar'))
+_paintera_version = os.getenv('PAINTERA_VERSION', __version__)
+_jfx_rt_jar       = os.getenv('JFX_RT_JAR', os.path.join(os.getenv('JAVA_HOME'), 'jre', 'lib', 'ext', 'jfxrt.jar'))
+
+assert os.path.exists(_jfx_rt_jar) and os.path.isfile(_jfx_rt_jar), 'JavaFX runtime jar does not exist ' \
+                                                                    'at default location %s. Please specify ' \
+                                                                    'JFX_RT_JAR environment variable.'
 
 
 def _init_jvm_options():
 
-    import imglyb_config
+    import scyjava_config
 
-    import jnius_config
-
-    PAINTERA_ENDPOINT    = 'org.janelia.saalfeldlab:paintera:{}'.format(__paintera_version__)
+    PAINTERA_ENDPOINT    = 'org.janelia.saalfeldlab:paintera:{}'.format(_paintera_version)
     RELEVANT_MAVEN_REPOS = {
                 'imagej.public' : 'https://maven.imagej.net/content/groups/public',
                 'saalfeldlab'   : 'https://saalfeldlab.github.io/maven'
-        }
+    }
+    for _, repo in scyjava_config.get_repositories().items():
+        if 'imagej.public' in RELEVANT_MAVEN_REPOS and repo == RELEVANT_MAVEN_REPOS['imagej.public']:
+            del RELEVANT_MAVEN_REPOS['imagej.public']
+        if 'saalfeldlab' in RELEVANT_MAVEN_REPOS and repo == RELEVANT_MAVEN_REPOS['saalfeldlab']:
+            del RELEVANT_MAVEN_REPOS['saalfeldlab']
 
-    imglyb_config.add_endpoints(PAINTERA_ENDPOINT, '{}'.format(os.getenv('PAINTERA_SLF4J_BINDING', 'org.slf4j:slf4j-simple:1.7.25')))
-    imglyb_config.add_repositories(RELEVANT_MAVEN_REPOS)
+    scyjava_config.add_endpoints(PAINTERA_ENDPOINT, '{}'.format(os.getenv('PAINTERA_SLF4J_BINDING', 'org.slf4j:slf4j-simple:1.7.25')))
+    scyjava_config.add_repositories(RELEVANT_MAVEN_REPOS)
 
-    JVM_OPTIONS_STR = 'JVM_OPTIONS'
+    scyjava_config.add_classpath(_jfx_rt_jar)
 
-    if JVM_OPTIONS_STR in os.environ:
-            jnius_config.add_options( *os.environ[ JVM_OPTIONS_STR ].split(' ') )
-                
-    jnius_config.add_classpath(__jfx_rt_jar)
+    return scyjava_config
 
-    return jnius_config
-
-jnius_config = _init_jvm_options()
+scyjava_config = _init_jvm_options()
 
 
